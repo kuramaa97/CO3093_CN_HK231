27e8cd65237d9c9f2f56127a82c1e78df47f51fe
Ntc <kuramaa97@gmail.com>
2023-11-26 16:34:46 +0700

    update

---
+++ client.py
@@ -0,0 +1,166 @@
+import socket
+import json
+import os
+import threading
+import shlex
+
+
+stop_event = threading.Event()
+
+def handle_file_request(conn, shared_files_dir):
+    try:
+        data = conn.recv(4096).decode()
+        command = json.loads(data)
+        if command['action'] == 'send_file':
+            lname = command['lname']
+            file_path = os.path.join(shared_files_dir, lname)
+            send_file_to_client(conn, file_path)
+    finally:
+        conn.close()
+
+def send_file_to_client(conn, file_path):
+    with open(file_path, 'rb') as f:
+        while True:
+            bytes_read = f.read(4096)
+            if not bytes_read:
+                break
+            conn.sendall(bytes_read)
+
+def start_host_service(port, shared_files_dir):
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_sock.bind(('0.0.0.0', port))
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_sock.listen()
+
+    while not stop_event.is_set():
+        try:
+            server_sock.settimeout(1) 
+            conn, addr = server_sock.accept()
+            thread = threading.Thread(target=handle_file_request, args=(conn, shared_files_dir))
+            thread.start()
+        except socket.timeout:
+            continue
+        except Exception as e:
+            break
+
+    server_sock.close()
+
+
+def publish_file(sock, lname, fname):
+    if not os.path.exists(lname):
+        print(f"Local file {lname} does not exist.")
+        return
+
+    hostname = socket.gethostname()
+    command = {
+        "action": "publish",
+        "fname": fname,
+        "lname": lname,
+        "hostname": hostname
+    }
+    sock.sendall(json.dumps(command).encode() + b'\n')
+    response = sock.recv(4096).decode()
+    print(response)
+
+
+
+def fetch_file(sock, fname):
+    command = {"action": "fetch", "fname": fname}
+    sock.sendall(json.dumps(command).encode() + b'\n')
+    response = json.loads(sock.recv(4096).decode())
+
+    if 'addresses' in response:
+        hosts = response['addresses']
+        host_info_str = "\n".join([f"{host['hostname']} at IP {host['ip']}" for host in hosts])
+        print(f"Hosts with the file {fname}:\n{host_info_str}")
+        if len(hosts) > 1:
+            chosen_ip = input("Enter the IP of the host to download from: ")
+            # Find the host entry with the chosen IP to get the corresponding lname
+            chosen_host = next((host for host in hosts if host['ip'] == chosen_ip), None)
+            if chosen_host:
+                request_file_from_peer(chosen_ip, chosen_host['lname'],fname)
+            else:
+                print("Invalid IP entered.")
+        elif hosts:
+            chosen_host = hosts[0]
+            request_file_from_peer(chosen_host['ip'], chosen_host['lname'],fname)
+        else:
+            print("No hosts have the file.")
+    else:
+        print("No peers have the file or the response format is incorrect.")
+
+
+def request_file_from_peer(ip_address, lname,fname):
+    peer_port = 65433  # The port where the peer service is expected to be running
+    peer_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        peer_sock.connect((ip_address, peer_port))
+        peer_sock.sendall(json.dumps({'action': 'send_file', 'lname': lname}).encode() + b'\n')
+  
+        # Proceed with file reception...
+   # Assume the peer is sending the file immediately after the request
+        with open(fname, 'wb') as f:
+            while True:
+                data = peer_sock.recv(4096)
+                if not data:  # No more data from peer
+                    break
+                f.write(data)
+
+        peer_sock.close()
+        print(f"File {fname} has been fetched from peer.")
+    except Exception as e:
+        print(f"An error occurred while connecting to peer at {ip_address}:{peer_port} - {e}")
+    finally:
+        peer_sock.close()
+
+
+
+
+def connect_to_server(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+
+    hostname = socket.gethostname()
+    sock.sendall(json.dumps({'action': 'introduce', 'hostname': hostname}).encode() + b'\n')
+
+    return sock
+
+
+
+def main(server_host, server_port):
+    # Start the host service thread
+    host_service_thread = threading.Thread(target=start_host_service, args=(65433, './'))
+    host_service_thread.start()
+
+    # Connect to the server
+    sock = connect_to_server(server_host, server_port)
+
+
+    try:
+        while True:
+            user_input = input("Enter command (publish lname fname, fetch fname, exit): ")
+            command_parts = shlex.split(user_input)
+
+            if len(command_parts) == 3 and command_parts[0].lower() == 'publish':
+                _, lname, fname = command_parts
+                publish_file(sock, lname, fname)
+            elif len(command_parts) == 2 and command_parts[0].lower() == 'fetch':
+                _, fname = command_parts
+                fetch_file(sock, fname)
+            elif user_input.lower() == 'exit':
+                stop_event.set()  # Signal all threads to stop
+                sock.close()
+                break  # Exit the main loop
+            else:
+                print("Invalid command.")
+    finally:
+            sock.close()
+            host_service_thread.join()  # Wait for the host service thread to finish
+
+
+
+
+if __name__ == "__main__":
+    SERVER_HOST = '192.168.1.14'  # Replace with your server's IP address
+    SERVER_PORT = 65432
+    main(SERVER_HOST, SERVER_PORT)
---
+++ server.py
@@ -0,0 +1,135 @@
+import logging
+import socket
+import threading
+import json
+import psycopg2
+import sys
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Establish a connection to the PostgreSQL database
+conn = psycopg2.connect(dbname="MMT", user="postgres", password="061203", host="localhost", port="4000")
+cur = conn.cursor()
+
+def log_event(message):
+    logging.info(message)
+
+def update_client_info(hostname, addr, fname, lname):
+    # Update the client's file list in the database
+    cur.execute(
+        f"""INSERT INTO client_files (lname, fname, hostname, address) VALUES (%s, %s, %s, %s) 
+            ON CONFLICT (address, fname, hostname) DO UPDATE SET lname = EXCLUDED.lname""",
+        (lname, fname, hostname, addr)
+    )
+    conn.commit()
+
+def client_handler(conn, addr):
+    try:
+
+        while True:
+            data = conn.recv(4096).decode()
+            log_event(f"Received data from {addr}: {data}")
+            if not data:
+                break
+
+            command = json.loads(data)
+
+            if command['action'] == 'publish':
+                hostname = command['hostname']
+                fname = command['fname']
+                lname= command['lname']
+                log_event(f"Updating client info in database for hostname: {hostname}")
+                update_client_info(hostname, addr[0], fname, lname)  # addr[0] is the IP address
+                log_event(f"Database update complete for hostname: {hostname}")
+                conn.sendall("File list updated successfully.".encode())
+
+            elif command['action'] == 'fetch':
+                fname = command['fname']
+                # Query the database for the IP addresses of the clients that have the file
+                cur.execute("SELECT DISTINCT ON (address, hostname, fname)   address, hostname, lname FROM client_files WHERE fname = %s", (fname,))
+                results = cur.fetchall()
+                if results:
+                    # Create a list of dictionaries with 'hostname' and 'ip' keyss
+                    peers_info = [{'hostname': hostname, 'ip': address, 'lname': lname} for address, hostname,lname in results]
+                    conn.sendall(json.dumps({'addresses': peers_info}).encode())
+                else:
+                    conn.sendall(json.dumps({'error': 'File not available'}).encode())
+
+
+
+            elif command['action'] == 'discover':
+                hostname = command['hostname']
+                cur.execute("SELECT fname FROM client_files WHERE hostname = %s", (hostname,))
+                files = cur.fetchall()
+                conn.sendall(json.dumps({'files': files}).encode())
+
+            elif command['action'] == 'ping':
+                hostname = command['hostname']
+                cur.execute("SELECT 1 FROM client_files WHERE hostname = %s LIMIT 1", (hostname,))
+                is_online = cur.fetchone() is not None
+                response = 'online' if is_online else 'offline'
+                conn.sendall(json.dumps({'status': response}).encode())
+
+    except Exception as e:
+        logging.exception(f"An error occurred while handling client {addr}: {e}")
+    finally:
+        conn.close()
+        log_event(f"Connection with {addr} has been closed.")
+
+def server_command_shell():
+    while True:
+        cmd_input = input("Server command: ")
+        cmd_parts = cmd_input.split()
+        if cmd_parts:
+            action = cmd_parts[0]
+            if action == "discover" and len(cmd_parts) == 2:
+                hostname = cmd_parts[1]
+                cur.execute("SELECT fname FROM client_files WHERE hostname = %s", (hostname,))
+                files = cur.fetchall()
+                print(f"Files for {hostname}: {files}")
+            elif action == "ping" and len(cmd_parts) == 2:
+                hostname = cmd_parts[1]
+                cur.execute("SELECT 1 FROM client_files WHERE hostname = %s LIMIT 1", (hostname,))
+                is_online = cur.fetchone() is not None
+                status = 'online' if is_online else 'offline'
+                print(f"Host {hostname} is {status}.")
+            elif action == "exit":
+                break
+            else:
+                print("Unknown command or incorrect usage.")
+
+def start_server(host='0.0.0.0', port=65432):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen()
+    log_event("Server started and is listening for connections.")
+
+    try:
+        while True:
+            conn, addr = server_socket.accept()
+            log_event(f"Accepted connection from {addr}")
+            thread = threading.Thread(target=client_handler, args=(conn, addr))
+            thread.start()
+            log_event(f"Active connections: {threading.active_count() - 1}")
+    except KeyboardInterrupt:
+        log_event("Server shutdown requested.")
+    finally:
+        server_socket.close()
+        cur.close()
+        conn.close()
+
+if __name__ == "__main__":
+    # Start server in a separate thread
+    server_thread = threading.Thread(target=start_server)
+    server_thread.start()
+
+    # Start the server command shell in the main thread
+    server_command_shell()
+
+    # Signal the server to shutdown
+    print("Server shutdown requested.")
+    # You would have some mechanism to properly shutdown the server here
+    # This could involve setting a shutdown flag that your threads check,
+    # or closing server sockets, etc.
+    sys.exit(0)
